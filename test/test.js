954ff836de94c5fb55d43fbc1fcad2fa42424fca
ingcaranton <ing.caranton@gmail.com>
2015-02-14 09:43:21 -0500

    Pruebas del contenido de about

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
+var app = require('../app');;
 var chai = require("chai");
+var chaiHttp = require('chai-http');
+var assert = chai.assert;
 var expect = chai.expect;
-var request = require('request');
+
+chai.use(chaiHttp);
 
 
-describe("rutas", function() {
-	describe("GET", function() {
-		describe('/index', function(){
-				it('error', function(done){
-			      	request.get('http://localhost:3000',function (error, response, body) {
-			      		expect(response.statusCode).to.be.below(400);
-			      		done();
-			      	});
+describe("about", function() {
+		describe('when i visit the about page', function(){
+				it('shows the about content', function(){
+			      	chai.request(app)
+  						.get ('/about')
+  						.end(function(err, res){
+  							expect(res.text).to.contain('Quienes somos');
+  							expect(res.text).to.contain("LA FUNDACIÓN CONEXIÓN");
+  						});
 			    });
 		});
-	});
 });

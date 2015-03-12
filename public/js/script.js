67097d1a6b39ea74bc1e634c062e109692778693
vicadi <vicadi7@gmail.com>
2015-03-12 08:30:55 -0500

    configuracion menu con boostrap y arreglo de botones

--- public/js/script.js
+++ public/js/script.js
@@ -55,10 +55,19 @@ $(document).ready(function() {
             return { init : init };
         })();
         Page.init();
-      });
+  });
   /*Fin Cargar Slider*/
   cargarPaginasEnLayout();
-  $( "#accordion" ).accordion();
+  
+  /*Acordeon*/
+  function toggleChevron(e) {
+    $(e.target)
+      .prev('.panel-heading')
+      .find("i.indicator")
+      .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+  }
+  $('#accordion').on('hidden.bs.collapse', toggleChevron);
+  $('#accordion').on('shown.bs.collapse', toggleChevron);  
 });
 
 /*Carga los enlaces de las paginas en el pie de pagina*/

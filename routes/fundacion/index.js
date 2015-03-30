3204d045a9e0d4311df61ea89f19b42abdc18aa0
ingcaranton <ing.caranton@gmail.com>
2015-03-30 17:51:43 -0500

    render hacer donacion

--- routes/fundacion/index.js
+++ routes/fundacion/index.js
@@ -54,9 +54,14 @@ app.route('/todocontenido')
 
 app.route('/hacerdonacion')
 .get(function(req, res){
+  var form={};
+  form.comercio="1";
+  form.json="MbgxlL0HYcKOx/3Ypdgo2xmmE7VxNlAe2mimlJV4DRNgXX99d3HAz2/YuRaaMMbKlcMiwGw6MbZxNUJyrx8rpRDqC9EKLWGJISdxCK5HHDsex9o3MH+CiuKEKclWY8LSbgX3h/gZ3IADvPmwAw/jJMAPdttqMrzXer5pnsxcVqyQihiz+k63Xq/T/oOK3PtOcykhw3sBVfygSHPhHH206pqP54Yw5Eomj/uEtnHRTrlZcpy99BrX/sk/3HRWC4t9JiScTqqd5ZdTiSdsEW2eMBphggraXruaNHYStguICNNhbQvKbpFRzO6KloHnvHlZnk9OsH9Gz8y52wM9mmds+Abyc7fUx1xzo7Sh4GfzCcoLjVjiwvGmWCU1WwEAqVI53gPwpIUwxfP8FfbcBKuBneyMiBan2vbc5MsLIHOwoIN5iODSwAWZ80ucFJhPbFe4P2qjLv7NBax1oXMQpJSjYUglrALTA7HYUZLRMmpP8jY/FEKKwfb9Qw==";
+  
   res.render('hacerdonacion',{
     message : req.flash('message'),
-    title : 'Donar'
+    title : 'Donar',
+    form: form
   });
 });
 
@@ -67,7 +72,7 @@ app.route('/donar')
   form.json2="MbgxlL0HYcKOx/3Ypdgo2xmmE7VxNlAe2mimlJV4DRNgXX99d3HAz2/YuRaaMMbKlcMiwGw6MbZxNUJyrx8rpRDqC9EKLWGJISdxCK5HHDsex9o3MH+CiuKEKclWY8LSbgX3h/gZ3IADvPmwAw/jJMAPdttqMrzXer5pnsxcVqyQihiz+k63Xq/T/oOK3PtOcykhw3sBVfygSHPhHH206pqP54Yw5Eomj/uEtnHRTrlZcpy99BrX/sk/3HRWC4t9JiScTqqd5ZdTiSdsEW2eMBphggraXruaNHYStguICNNhbQvKbpFRzO6KloHnvHlZnk9OsH9Gz8y52wM9mmds+Abyc7fUx1xzo7Sh4GfzCcoLjVjiwvGmWCU1WwEAqVI53gPwpIUwxfP8FfbcBKuBneyMiBan2vbc5MsLIHOwoIN5iODSwAWZ80ucFJhPbFe4P2qjLv7NBax1oXMQpJSjYUglrALTA7HYUZLRMmpP8jY/FEKKwfb9Qw==";
   form.json=JSON.stringify({"json":{"idComercio":1,"idTipoTransaccion":1,"pagos":1,"idProducto":"1001","idConcepto":"prueba Transaccion",
   "concepto":"prueba Transaccion","valor":2320.00,"impuesto":16,"valorImpuesto":320,"moneda":"COP","secureCode":"ed81fadcaf",
-  "certificado":"DXqxJP8vnxhfdJKbmMtwUw==","urlComercio":"http://192.155.93.202/phoneapp/post.php" ,
+  "certificado":"DXqxJP8vnxhfdJKbmMtwUw==","urlComercio":"https://mysterious-ridge-6587.herokuapp.com/donar" ,
   "urlConfirmacion":"https://mysterious-ridge-6587.herokuapp.com/donar"}}) ;
 
   var b = new Buffer(form.json2, 'base64')
@@ -80,12 +85,8 @@ app.route('/donar')
 
   form.comercio=new Buffer(form.comercio).toString('base64');
   res.render('donar',{
-    form : form,
-    title : "pruevba"
+    form : form
   });
-})
-.post(function(req, res) {
-  res.send("dasdasdasdas"+req.body+"si llego");
 });
 
 app.route('/:pagina')
